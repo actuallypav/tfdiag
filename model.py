34c1b6d847bb52c23f9cad20249701bfa89bc9fe
actuallypav <professional.extras708@passinbox.com>
2025-07-01 16:00:49 +0100

    updated model

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 class TerraformResource:
-    def __init__(self, type, name, properties):
+    def __init__(self, type, name, properties, depends_on=None):
         self.type = type
         self.name = name
         self.properties = properties
-        self.depends_on = []
+        self.depends_on = depends_on if depends_on is not None else []
+    
+    def full_id(self):
+        return f"{self.type}.{self.name}"
